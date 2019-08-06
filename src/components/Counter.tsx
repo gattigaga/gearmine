a087206a59235f8f861feaa13c9d2e37b8a9c4b9
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2019-08-06 20:03:45 +0700

    Create Counter component

---
+++ src/components/Counter.tsx
@@ -0,0 +1,69 @@
+import React, { FC, useState, useEffect } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 136px;
+  height: 36px;
+  border: 1px solid #ccc;
+  display: flex;
+`;
+
+const ButtonLeft = styled.button`
+  width: 32px;
+  height: 100%;
+  border: 0px;
+  border-right: 1px solid #ccc;
+  background: #eee;
+`;
+
+const ButtonRight = styled(ButtonLeft)`
+  border: 0px;
+  border-left: 1px solid #ccc;
+`;
+
+const Input = styled.input`
+  width: 72px;
+  height: 100%;
+  box-sizing: border-box;
+  text-align: center;
+  border: 0px;
+`;
+
+type Props = {
+  onDecrement?: (value: number) => void;
+  onIncrement?: (value: number) => void;
+};
+
+const Counter: FC<Props> = ({ onDecrement, onIncrement }) => {
+  const [value, setValue] = useState(1);
+
+  const increase = (step: number) => {
+    setValue(prevValue => prevValue + step);
+  };
+
+  const add = () => {
+    const incrementor = 1;
+    const newValue = value + incrementor;
+
+    increase(incrementor);
+    onIncrement && onIncrement(newValue);
+  };
+
+  const substract = () => {
+    const incrementor = value > 1 ? -1 : 0;
+    const newValue = value + incrementor;
+
+    value > 1 && increase(incrementor);
+    onDecrement && onDecrement(newValue);
+  };
+
+  return (
+    <Container>
+      <ButtonLeft onClick={substract}>-</ButtonLeft>
+      <Input type="text" min="1" value={value} />
+      <ButtonRight onClick={add}>+</ButtonRight>
+    </Container>
+  );
+};
+
+export default Counter;
