be6eaaacee7910c4bfda9f6af6dc7faf773871b4
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2019-08-09 19:37:43 +0700

    Create products by category page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import ProductDetail from "./pages/ProductDetail";
 import ProductsByKeyword from "./pages/ProductsByKeyword";
+import ProductsByCategory from "./pages/ProductsByCategory";
 
 const App: FC = () => {
   const search = (keyword: string) => {
@@ -20,6 +21,7 @@ const App: FC = () => {
         <Cart path="/cart" />
         <ProductDetail path="/product/:slug" />
         <ProductsByKeyword path="/search" />
+        <ProductsByCategory path="/category/:slug" />
       </Router>
     </div>
   );
---
+++ src/pages/ProductsByCategory.tsx
@@ -0,0 +1,71 @@
+import React, { FC } from "react";
+import { Helmet } from "react-helmet";
+import styled from "styled-components";
+import uuid from "uuid/v4";
+import queryString from "query-string";
+
+import Footer from "../components/Footer";
+import Product from "../components/Product";
+
+const Wrapper = styled.div`
+  width: 1080px;
+  margin: auto;
+  display: flex;
+  padding: 32px 0px;
+`;
+
+const Section = styled.section`
+  margin-bottom: 64px;
+`;
+
+const Info = styled.p`
+  font-family: "Roboto";
+  color: #333;
+  margin-top: 0px;
+`;
+
+const ProductWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 200px);
+  grid-gap: 20px;
+`;
+
+type Props = {
+  path: string;
+};
+
+const ProductsByCategory: FC<Props> = () => {
+  const productsPerRow = 5;
+
+  const products = [...Array(productsPerRow * 3)].map(() => ({
+    id: uuid(),
+    image: "https://via.placeholder.com/200x200?text=Dummy",
+    name: "iPhone X 64GB - Elegant Black",
+    price: 12000000,
+    href: "/"
+  }));
+
+  return (
+    <div>
+      <Helmet>
+        <title>Gearmine | #1 Gadget Shop</title>
+      </Helmet>
+      <Wrapper>
+        <Section>
+          <Info>
+            There is <strong>15.323</strong> items in category{" "}
+            <strong>"Smartphone"</strong>
+          </Info>
+          <ProductWrapper>
+            {products.map(product => (
+              <Product key={product.id} {...product} />
+            ))}
+          </ProductWrapper>
+        </Section>
+      </Wrapper>
+      <Footer />
+    </div>
+  );
+};
+
+export default ProductsByCategory;
